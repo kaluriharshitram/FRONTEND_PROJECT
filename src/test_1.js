f950f449e57ece16420b8d441bf9fcb54c849739
Harshitram <kaluriharshitram@gmail.com>
2024-02-16 22:32:23 +0530

    my first commit

---
+++ src/test_1.js
@@ -0,0 +1,38 @@
+// for(let i=0;i<=10;i=i+5){
+//     console.log(i,"value of i")
+// }
+
+// let grade='B';
+// switch(grade){
+//     case 'A':console.log("student is first");break;
+//     case 'B':console.log("student is second");break;
+//     case 'C':console.log("student is third");break;
+//     default:console.log("please mention correct grade");break;
+// }
+
+// while loop
+// let i=1;
+// while(i<5){
+//     console.log(i);
+// i++;
+// }
+
+//pattern
+// let pattern=""
+// for(let i=0;i<4; i++){
+//     pattern = pattern + "*"
+//     console.log(pattern)
+// }
+
+//Arthematic operators
+//Number
+//addition
+// let A = true + 13
+// console.log(A)
+// console.log(typeof A)
+
+//remainder
+//console.log(10 ** 3)
+
+//exponent
+console.log(10 ** 3)
